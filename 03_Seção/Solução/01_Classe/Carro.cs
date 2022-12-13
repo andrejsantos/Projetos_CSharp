70b222f3a449ddd0fd19ed7ee3b4b98beac0865a
andrejsantos <andrejesus867@gmail.com>
2022-12-12 22:05:45 -0300

    POO

---
+++ 03_Seção/Solução/01_Classe/Carro.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using System.Reflection.PortableExecutable;
+
+namespace _01_Classe
+{
+    class Carro
+    {
+        string marca = "chefrole"; //Toda vez que eu me referir essa clsse ele já vem com essa "Pré definição", podendo alterar mais para frente.
+        string modelo;
+        string cor;
+    }
+}
