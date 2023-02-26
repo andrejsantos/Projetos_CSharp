859bce8d9fe1e8e7f621323d44fad45814d8dee6
andrejsantos <andrejesus867@gmail.com>
2023-02-26 16:01:07 -0300

    Novas aulas da ALURA

---
+++ Aulas/Aulas/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); //Manda de imprimir algo na tela
+
+
+int idade; // Indicando o tipo de variavel
+
+idade = 12; // Colocanodo valor na variavel
+
+Console.WriteLine("Sua idade é:" + idade);// Chamando a variavel e concatenado a maesma
+Console.WriteLine("Tecle enter para fechar...");
+Console.ReadLine();
