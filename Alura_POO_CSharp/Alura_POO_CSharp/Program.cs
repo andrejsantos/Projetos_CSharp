ed65f8d205e1c789469400c02a66363bacf1a6d9
andrejsantos <andrejesus867@gmail.com>
2023-03-02 19:31:55 -0300

    Criando uma classe e um objeto

--- Alura_POO_CSharp/Alura_POO_CSharp/Program.cs
+++ Alura_POO_CSharp/Alura_POO_CSharp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bank;// Chamando classe
+
+ContaCorrente contadoAndré = new ContaCorrente(); //Criando objeto
+contadoAndré.titular = "André de Jesus";
+contadoAndré.numero_agencia = 15;
+contadoAndré.conta = "1010-X";
+contadoAndré.saldo = 100;
+
+Console.WriteLine("Seu saldo é de R$: " + contadoAndré.saldo);
+Console.ReadLine();
