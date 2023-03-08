41001b9c698c8d944df75827b8f5c6f25dc35823
andrejsantos <andrejesus867@gmail.com>
2023-03-07 22:19:54 -0300

    Terminando mais um modulo do curso
    Aprendendo o que é namespace, e classes

--- Alura_POO_CSharp/Alura_POO_CSharp/Contas/ContaCorrente.cs
+++ Alura_POO_CSharp/Alura_POO_CSharp/Contas/ContaCorrente.cs
@@ -1,33 +1,35 @@
-﻿using System;
+﻿using Bank.Titular;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bank
+namespace Bank.Contas
 {
     public class ContaCorrente
     {
         public int numero_agencia;
         public string conta;
-        public string titular;
         public double saldo;
+        public Cliente titular; // Colocando referencia da outra classe na mesma /*Usando o NameSpace para pegar a classe*/
 
         public void Depositar(double valor)// Definindo um metodo publico
         {
-            this.saldo += valor;
+            saldo += valor;
         }
 
         public void Sacar(double valor)
         {
-            if (saldo > valor) {
-                this.saldo -= valor;
+            if (saldo > valor)
+            {
+                saldo -= valor;
             }
             else
             {
                 Console.WriteLine($"Impossivel sacar, Você só tem R$:{valor:0.00} na conta");
             }
-            
+
         }
 
         public bool transferir(double valor, ContaCorrente destino)
@@ -42,7 +44,7 @@ namespace Bank
                 destino.saldo += valor;
                 return true;
             }
-            
+
         }
     }
 }
--- Alura_POO_CSharp/Alura_POO_CSharp/Program.cs
+++ Alura_POO_CSharp/Alura_POO_CSharp/Program.cs
@@ -1,47 +1,67 @@
-﻿using Bank;// Chamando classe
+﻿using Bank.Contas; // NameSpaces
+using Bank.Titular;// Chamando classe
 
-ContaCorrente contadoAndré = new ContaCorrente(); //Criando objeto
-contadoAndré.titular = "André de J. Santos";//Campos do objetos - (Publicos)
-contadoAndré.numero_agencia = 15;
-contadoAndré.conta = "1010-X";
-contadoAndré.saldo = 100;
+//ContaCorrente contadoAndré = new ContaCorrente(); //Criando objeto
+//contadoAndré.titular = "André de J. Santos";//Campos do objetos - (Publicos)
+//contadoAndré.numero_agencia = 15;
+//contadoAndré.conta = "1010-X";
+//contadoAndré.saldo = 100;
 
-ContaCorrente contaFernanda = new ContaCorrente();
-contaFernanda.titular = "Fernanda Kelly";
-contaFernanda.numero_agencia = 15;
-contaFernanda.conta = "1011-X";
-contaFernanda.saldo = 500;
+//ContaCorrente contaFernanda = new ContaCorrente();
+//contaFernanda.titular = "Fernanda Kelly";
+//contaFernanda.numero_agencia = 15;
+//contaFernanda.conta = "1011-X";
+//contaFernanda.saldo = 500;
 
-// --------------- Conta André -----------
-Console.WriteLine("--------------------------------------");
-Console.WriteLine($"Titular:{contadoAndré.titular}");
-Console.WriteLine($"Agencia:{contadoAndré.numero_agencia}");
-Console.WriteLine($"Numero da conta:{contadoAndré.conta}");
-Console.WriteLine($"Saldo atual:{contadoAndré.saldo}");
-Console.WriteLine("--------------------------------------");
-Console.WriteLine($"Depositando R$:{100:0.00}");
-contadoAndré.Depositar(100);
+//// --------------- Conta André -----------
+//Console.WriteLine("--------------------------------------");
+//Console.WriteLine($"Titular:{contadoAndré.titular}");
+//Console.WriteLine($"Agencia:{contadoAndré.numero_agencia}");
+//Console.WriteLine($"Numero da conta:{contadoAndré.conta}");
+//Console.WriteLine($"Saldo atual:{contadoAndré.saldo}");
+//Console.WriteLine("--------------------------------------");
+//Console.WriteLine($"Depositando R$:{100:0.00}");
+//contadoAndré.Depositar(100);
 
-Console.WriteLine($"Saldo atual R$:{contadoAndré.saldo:0.00}");//Formatando os numeros
+//Console.WriteLine($"Saldo atual R$:{contadoAndré.saldo:0.00}");//Formatando os numeros
 
-Console.WriteLine($"Sacando R$:{300:0.00}");
-contadoAndré.Sacar(300);
+//Console.WriteLine($"Sacando R$:{300:0.00}");
+//contadoAndré.Sacar(300);
 
-Console.WriteLine($"Saldo atual R$:{contadoAndré.saldo:0.00} ");
+//Console.WriteLine($"Saldo atual R$:{contadoAndré.saldo:0.00} ");
 
-// ------------ Conta Fernanda ----------
-Console.WriteLine("--------------------------------------");
-Console.WriteLine($"Titular:{contaFernanda.titular}");
-Console.WriteLine($"Agencia:{contaFernanda.numero_agencia}");
-Console.WriteLine($"Numero da conta:{contaFernanda.conta}");
-Console.WriteLine($"Saldo atual:{contaFernanda.saldo}");
-Console.WriteLine("--------------------------------------");
-Console.WriteLine($"Trasferindo R$:{250:0.00} ");
-contaFernanda.transferir(250, contadoAndré);
+//// ------------ Conta Fernanda ----------
+//Console.WriteLine("--------------------------------------");
+//Console.WriteLine($"Titular:{contaFernanda.titular}");
+//Console.WriteLine($"Agencia:{contaFernanda.numero_agencia}");
+//Console.WriteLine($"Numero da conta:{contaFernanda.conta}");
+//Console.WriteLine($"Saldo atual:{contaFernanda.saldo}");
+//Console.WriteLine("--------------------------------------");
+//Console.WriteLine($"Trasferindo R$:{250:0.00} ");
+//contaFernanda.transferir(250, contadoAndré);
 
-Console.WriteLine($"Saldo atual R$:{contaFernanda.saldo:0.00}");
+//Console.WriteLine($"Saldo atual R$:{contaFernanda.saldo:0.00}");
 
-Console.WriteLine("--------------------------------------");
+//Console.WriteLine("--------------------------------------");
 
-Console.WriteLine($"Saldo atual 'André' R$:{contadoAndré.saldo:0.00}");
-Console.ReadLine();
+//Console.WriteLine($"Saldo atual 'André' R$:{contadoAndré.saldo:0.00}");
+//Console.ReadLine();
+
+
+Cliente cliente = new Cliente();
+cliente.nome = "André de J. Santos";
+cliente.cpf = "1234567-89";
+cliente.profissao = "Analista de sistemas";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numero_agencia = 1;
+conta.saldo = 100;
+
+Console.WriteLine($"Titular: {conta.titular.nome}");//Navegando entre objetos
+Console.WriteLine($"CPF: {conta.titular.cpf}");
+Console.WriteLine($"Profissão: {conta.titular.profissao}");
+Console.WriteLine($"N° Conta: {conta.conta}");
+Console.WriteLine($"N° Agencia: {conta.numero_agencia}");
+Console.WriteLine($"Saldo: {conta.saldo}");
