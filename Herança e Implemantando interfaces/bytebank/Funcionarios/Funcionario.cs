eb495fefee32564d0d83c1b50ace52a1d85abca9
andrejsantos <andrejesus867@gmail.com>
2023-03-27 22:52:48 -0300

    Começando mais um projeto

---
+++ Herança e Implemantando interfaces/bytebank/Funcionarios/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public double Salario { get; set; }
+
+        public double Getbonificacao()
+        {
+            return this.Salario * 0.10;
+        }
+        
+    }
+}
---
+++ Herança e Implemantando interfaces/bytebank/Program.cs
@@ -0,0 +1,14 @@
+﻿using bytebank.Funcionarios;
+
+Funcionario André = new Funcionario();
+André.Nome = "André de Jesus Santos";
+André.CPF = "390075668-45";
+André.Salario = 2000;
+
+Console.WriteLine("----CADASTRO DE FUNCIONARIO---");
+Console.WriteLine($"Funcionario: {André.Nome}");
+Console.WriteLine($"CPF: {André.CPF}");
+Console.WriteLine($"Salario: {André.Salario:00.00}");
+Console.WriteLine($"Sua Bonificação é: {André.Getbonificacao()}");
+
+Console.ReadKey();
